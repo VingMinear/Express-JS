e9feb13ca9b90d812aadb5bc02520e80685b4036
N E A <minearving@gmail.com>
2023-04-24 21:35:23 +0700

    git push

--- auth/login.js
+++ auth/login.js
@@ -3,7 +3,24 @@ const router=express.Router();
 
 
 router.post('/',(req,res)=>{
-    
-
-    res.json({});
+    const {phone}=req.body;
+    if(phone==null){
+        res.json({
+            message:"body required",
+            req:req.body,
+        });
+    }else if(phone!=0974278786){
+        res.json({
+            message:"phone number is invalid"
+        });
+    }else{
+        res.json({
+            phone:req.body,
+            message:"login successful",
+            token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+            refresh_token:"wRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+        });
+    }
+   
 });
+module.exports=router;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const loginRoute=require('./auth/login');
+
+//----------------------------------------------------------------
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded());
 const port=8080;
 app.listen(port,()=>{
     console.log("Server runing on port " + port);
 },);
+
+//----------------------------------------------------------------
+
+app.use('/login',loginRoute);
